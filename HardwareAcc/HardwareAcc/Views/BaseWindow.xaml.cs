288bca7dcb82805a415f5f279be0218fc1fcb233
Leadley Guide <leadley.guide@rainbow-factory.xyz>
2024-06-13 22:06:00 +0500

    Implement design

--- HardwareAcc/HardwareAcc/Views/BaseWindow.xaml.cs
+++ HardwareAcc/HardwareAcc/Views/BaseWindow.xaml.cs
@@ -4,4 +4,18 @@ namespace HardwareAcc.Views;
 
 public partial class BaseWindow : Window
 {
+    private void Minimize_Click(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Maximize_Click(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
 }
