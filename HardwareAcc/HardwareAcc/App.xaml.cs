5b3f048a2f2ea3bf2be0a164ad88547e99af4297
Leadley Guide <leadley.guide@rainbow-factory.xyz>
2024-06-15 20:46:36 +0500

    Implement Di in App.cs, remove StartupUri

--- HardwareAcc/HardwareAcc/App.xaml.cs
+++ HardwareAcc/HardwareAcc/App.xaml.cs
@@ -1,8 +1,38 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using HardwareAcc.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HardwareAcc
 {
     public partial class App : Application
     {
+        private IServiceProvider? _serviceProvider;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ServiceCollection serviceCollection = new();
+            RegisterServices(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+
+            TestWindow testWindow = _serviceProvider.GetRequiredService<TestWindow>();
+            testWindow.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (_serviceProvider is IDisposable disposable) 
+                disposable.Dispose();
+            
+            base.OnExit(e);
+        }
+        
+        private void RegisterServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<TestWindow>();
+            
+        }
     }
 }
