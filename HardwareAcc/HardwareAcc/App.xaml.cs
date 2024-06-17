087274e694bf4cebaf88f4bd28495177983c4a88
Leadley Guide <leadley.guide@rainbow-factory.xyz>
2024-06-17 09:42:10 +0500

    Clean up

--- HardwareAcc/HardwareAcc/App.xaml.cs
+++ HardwareAcc/HardwareAcc/App.xaml.cs
@@ -1,54 +1,16 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
-using System.Text;
 using System.Windows;
 using HardwareAcc.Services.DBConnectionService;
 using HardwareAcc.Views;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using MySqlConnector;
 
 namespace HardwareAcc
 {
     public partial class App
     {
         private IServiceProvider? _serviceProvider;
-
-        private void TEST_DB_CONNECTION(IServiceProvider serviceProvider)
-        {
-            IDBConnectionService dbConnectionService = serviceProvider.GetRequiredService<IDBConnectionService>();
-            using MySqlConnection connection = dbConnectionService.GetConnection();
-
-            using var command = new MySqlCommand("SELECT * FROM hardware;", connection);
-            using var reader = command.ExecuteReader();
-
-            string output;
-            
-            if (!reader.HasRows)
-            {
-                output = "No rows found.";
-            }
-
-            var result = new StringBuilder();
-            while (reader.Read())
-            {
-                for (var i = 0; i < reader.FieldCount; i++)
-                {
-                    result.Append(reader.GetName(i) + ": " + reader.GetValue(i) + "\t");
-                }
-                result.AppendLine();
-            }
-
-            output = result.ToString();
-            
-            Debug.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-            Debug.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-            Debug.WriteLine(output);
-            Debug.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-            Debug.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-        }
-        
         
         protected override void OnStartup(StartupEventArgs e)
         {
@@ -62,10 +24,6 @@ namespace HardwareAcc
 
             TestWindow testWindow = _serviceProvider.GetRequiredService<TestWindow>();
             testWindow.Show();
-
-
-
-            TEST_DB_CONNECTION(_serviceProvider);
         }
 
         protected override void OnExit(ExitEventArgs e)
