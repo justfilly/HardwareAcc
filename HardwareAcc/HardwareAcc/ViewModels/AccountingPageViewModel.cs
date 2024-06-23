56a9bbee8511935001db7287b7febcdd88ec9bf3
Steddy Stride <steddy.stride@rainbow-factory.xyz>
2024-06-23 18:41:42 +0500

    Implement active TabButton

--- HardwareAcc/HardwareAcc/ViewModels/AccountingPageViewModel.cs
+++ HardwareAcc/HardwareAcc/ViewModels/AccountingPageViewModel.cs
@@ -9,18 +9,41 @@ namespace HardwareAcc.ViewModels;
 
 public class AccountingPageViewModel : BaseViewModel
 {
-    private IServiceProvider _serviceProvider;
-    private IViewLocator _viewLocator;
+    private readonly IServiceProvider _serviceProvider;
+    private readonly IViewLocator _viewLocator;
     
     public AccountingPageViewModel(IServiceProvider serviceProvider, IViewLocator viewLocator)
     {
         _serviceProvider = serviceProvider;
         _viewLocator = viewLocator;
         
-        HardwareTabCommand = new RelayCommand(SwitchTab<HardwareTabPageViewModel>);
-        UsersTabCommand = new RelayCommand(SwitchTab<UsersTabPageViewModel>);
-        AudiencesTabCommand = new RelayCommand(SwitchTab<AudiencesTabPageViewModel>);
-        StatusesTabCommand = new RelayCommand(SwitchTab<StatusesTabPageViewModel>);
+        HardwareTabCommand = new RelayCommand(() =>
+        {
+            ClearActiveTab();
+            IsHardwareTabActive = true;
+            SwitchTab<HardwareTabPageViewModel>();
+        });
+        
+        UsersTabCommand = new RelayCommand(() =>
+        {
+            ClearActiveTab();
+            IsUsersTabActive = true;
+            SwitchTab<UsersTabPageViewModel>();
+        });
+        
+        AudiencesTabCommand = new RelayCommand(() =>
+        {
+            ClearActiveTab();
+            IsAudiencesTabActive = true;
+            SwitchTab<AudiencesTabPageViewModel>();
+        });
+        
+        StatusesTabCommand = new RelayCommand(() =>
+        {
+            ClearActiveTab();
+            IsStatusesTabActive = true;
+            SwitchTab<StatusesTabPageViewModel>();
+        });
         
         SwitchTab<HardwareTabPageViewModel>();
     }
@@ -43,6 +66,54 @@ public class AccountingPageViewModel : BaseViewModel
         }
     }
 
+    private bool _isHardwareTabActive;
+    public bool IsHardwareTabActive
+    {
+        get => _isHardwareTabActive;
+    
+        set
+        {
+            _isHardwareTabActive = value;
+            OnPropertyChanged(nameof(IsHardwareTabActive));
+        }
+    }
+    
+    private bool _isUsersTabActive;
+    public bool IsUsersTabActive
+    {
+        get => _isUsersTabActive;
+    
+        set
+        {
+            _isUsersTabActive = value;
+            OnPropertyChanged(nameof(IsUsersTabActive));
+        }
+    }
+    
+    private bool _isAudiencesTabActive;
+    public bool IsAudiencesTabActive
+    {
+        get => _isAudiencesTabActive;
+    
+        set
+        {
+            _isAudiencesTabActive = value;
+            OnPropertyChanged(nameof(IsAudiencesTabActive));
+        }
+    }
+    
+    private bool _isStatusesTabActive;
+    public bool IsStatusesTabActive
+    {
+        get => _isStatusesTabActive;
+    
+        set
+        {
+            _isStatusesTabActive = value;
+            OnPropertyChanged(nameof(IsStatusesTabActive));
+        }
+    }
+    
     private void SwitchTab<TViewModel>() where TViewModel : BaseViewModel
     {
         TViewModel viewModel = _serviceProvider.GetRequiredService<TViewModel>();
@@ -52,5 +123,13 @@ public class AccountingPageViewModel : BaseViewModel
         view.DataContext = viewModel;
         
         TabPage = view;
+    }
+
+    private void ClearActiveTab()
+    {
+        IsHardwareTabActive = false;
+        IsUsersTabActive = false;
+        IsAudiencesTabActive = false;
+        IsStatusesTabActive = false;
     }
 }
