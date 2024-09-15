ab35a5471c221f865e1279c2cd1c3c64f632320a
Leadley Guide <leadley.guide@rainbow-factory.xyz>
2024-09-15 18:08:33 +0500

    Add View & ViewModel

--- HardwareAcc/HardwareAcc/ViewModels/Tabs/AudiencesTabPageViewModel.cs
+++ HardwareAcc/HardwareAcc/ViewModels/Tabs/AudiencesTabPageViewModel.cs
@@ -1,6 +1,29 @@
+using System.Collections.ObjectModel;
+using HardwareAcc.Models;
+
 namespace HardwareAcc.ViewModels.Tabs;
 
 public class AudiencesTabPageViewModel : BaseViewModel
 {
+    public AudiencesTabPageViewModel()
+    {
+        _audiences = new ObservableCollection<Audience>()
+        {
+            new() { Id = 1, Name = "Computer Science Class", Code = "A423" },
+            new() { Id = 2, Name = "Chemistry lab", Code = "A107" },
+            new() { Id = 3, Name = "Web design class", Code = "A210" },
+        };
+    }
     
+    private ObservableCollection<Audience> _audiences;
+    public ObservableCollection<Audience> Audiences
+    {
+        get => _audiences;
+    
+        set
+        {
+            _audiences = value;
+            OnPropertyChanged(nameof(Audiences));
+        }
+    }
 }
