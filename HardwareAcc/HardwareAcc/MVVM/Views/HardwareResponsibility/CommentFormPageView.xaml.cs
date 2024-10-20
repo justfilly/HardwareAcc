bba82c7d0b568c6a377999cc4ac5be3c34d378cc
Leadley Guide <leadley.guide@rainbow-factory.xyz>
2024-10-20 19:20:40 +0500

    Add MultilineTextInputField

---
+++ HardwareAcc/HardwareAcc/MVVM/Views/HardwareResponsibility/CommentFormPageView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace HardwareAcc.MVVM.Views.HardwareResponsibility;
+
+public partial class CommentFormPageView : Page
+{
+    public CommentFormPageView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ HardwareAcc/HardwareAcc/MVVM/Views/UserControls/MultilineTextInputField.xaml.cs
@@ -0,0 +1,38 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HardwareAcc.MVVM.Views.UserControls;
+
+public partial class MultilineTextInputField : UserControl
+{
+    public MultilineTextInputField()
+    {
+        InitializeComponent();
+    }
+    
+    public static readonly DependencyProperty InputTextDependencyProperty =
+        DependencyProperty.Register(
+            name:nameof(InputText),
+            propertyType:typeof(string),
+            ownerType:typeof(TextInputField),
+            new PropertyMetadata(string.Empty));
+    
+    public string InputText
+    {
+        get => (string)GetValue(InputTextDependencyProperty);
+        set => SetValue(InputTextDependencyProperty, value);
+    }
+    
+    public static readonly DependencyProperty LabelTextDependencyProperty =
+        DependencyProperty.Register(
+            name: nameof(LabelText),
+            propertyType: typeof(string),
+            ownerType: typeof(TextInputField),
+            new PropertyMetadata(string.Empty));
+
+    public string LabelText
+    {
+        get => (string)GetValue(LabelTextDependencyProperty);
+        set => SetValue(LabelTextDependencyProperty, value);
+    }
+}
