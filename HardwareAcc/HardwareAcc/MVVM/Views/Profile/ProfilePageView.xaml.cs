734d113e2c9140186afa715c1496c07c4c301218
Leadley Guide <leadley.guide@rainbow-factory.xyz>
2024-10-31 14:56:33 +0500

    Add ProfilePageView

---
+++ HardwareAcc/HardwareAcc/MVVM/Views/Profile/ProfilePageView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace HardwareAcc.MVVM.Views.Profile;
+
+public partial class ProfilePageView : Page
+{
+    public ProfilePageView()
+    {
+        InitializeComponent();
+    }
+}
